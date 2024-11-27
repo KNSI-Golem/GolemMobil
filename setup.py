4fd3701053cab7660ed8deb7a0d3a894a2b08c52
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-11-27 16:23:49 +0100

    fix: Added setup.py file to enable instaling project itself

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GolemMobil',
+    version='0.1',
+    packages=find_packages(),
+    description='Educational project to learn AI',
+    author='Golem',
+)
